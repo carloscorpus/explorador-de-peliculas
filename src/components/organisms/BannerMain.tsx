7dc4831a8896f2525914e6bdcd5cf0509e228fba
Carlos corpus susanibar <xroberx01@hotmail.com>
2025-05-27 22:23:01 -0500

    Refactor: se a mejorado las imagenes not found

--- src/components/organisms/BannerMain.tsx
+++ src/components/organisms/BannerMain.tsx
@@ -35,6 +35,9 @@ export const BannerMain = () => {
 							title={data.Title}
 							image={data.Poster}
 							sx={{ height: { xs: 320, md: '100%' }, minWidth: { sm: 220 }, maxWidth: { sm: 320 } }}
+							onError={(e) => {
+								e.currentTarget.src = `https://placehold.co/600x400?text=${data.Title} :(`;
+							}}
 						/>
 
 						<Box>
